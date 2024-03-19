02eea333df5de511d050580326d836d2b9a3fc27
Serhii Veretennikov <gridis72@gmail.com>
2024-03-19 17:08:29 +0100

    finished user agent

--- app/server.c
+++ app/server.c
@@ -7,6 +7,8 @@
 #include <errno.h>
 #include <unistd.h>
 
+int send_five_hundred(int client_fd, int server_fd);
+
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
@@ -60,6 +62,7 @@ int main() {
 	printf("Client connected\n");
 
 	char read_buffer[1024];
+	char read_buffer_copy[1024]; 
 
 	int bytes_recieved = recv(client_fd, read_buffer, 1024, 0);
 	if (bytes_recieved == -1) {
@@ -67,15 +70,15 @@ int main() {
 		return 1;
 	}
 
+	printf("Read buffer is %s\n", read_buffer);
+	strcpy(read_buffer_copy, read_buffer);
+
 	//Extract path
-	char *http_method = strtok(read_buffer, " ");
+	char *http_method = strtok(read_buffer_copy, " ");
 	char *path = strtok(0, " ");
 
 	printf("Path is %s\n", path);
-	printf("Path length is %lu\n", strlen(path));
-
-	char *data = strstr(path, "/echo/");
-	printf("Data is %s\n", data);
+	//printf("Path length is %lu\n", strlen(path));
 
 	if (!strcmp(path, "/")) {
 		int bytes_sent = send(client_fd, "HTTP/1.1 200 OK\r\n\r\n", 19, 0);
@@ -86,27 +89,49 @@ int main() {
 
 		printf("Response sent\n");
 
-	} else if (data != 0) {
-		char *content = data + strlen("/echo/");
+	} else if (strstr(path, "/echo/")) {
 		char response[1024];
+		char *content = strstr(path, "/echo/") + strlen("/echo/");
 
 		printf("Content is %s\n", content);
 
-		if (sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %lu\r\n\r\n%s", strlen(content), content) < 0) {
+		int response_len = sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %lu\r\n\r\n%s", strlen(content), content);
+
+		if (response_len < 0) {
 			printf("Sprint failed\n");
-			int bytes_sent = send(client_fd, "HTTP/1.1 500 Internal Server Error\r\n\r\n", 33, 0);
-			if (bytes_sent == -1) {
-				printf("Send failed\n");
-				return 1;
-			}
-
-			printf("Response sent\n");
-
-			close(server_fd);
+			send_five_hundred(client_fd, server_fd);
 			return 1;
 		}
 
-		int bytes_sent = send(client_fd, response, strlen(response), 0);
+		int bytes_sent = send(client_fd, response, response_len, 0);
+		if (bytes_sent == -1) {
+			printf("Send failed\n");
+			return 1;
+		}
+
+		printf("Response sent\n");
+	} else if (strstr(path, "/user-agent") != 0){ 
+		char user_agent[128];
+		char response[1024];
+		char *user_agent_read = strstr(read_buffer, "User-Agent: ") + strlen("User-Agent: ");
+		int i = 0;
+
+		while (user_agent_read[i] != '\r' && user_agent_read[i + 1] != '\n')
+			i++;
+
+		strncpy(user_agent, user_agent_read, i);
+
+		printf("User-Agent is %s\n", user_agent);
+
+		int response_len = sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %lu\r\n\r\n%s", strlen(user_agent), user_agent);
+
+		if (response_len < 0) {
+			printf("Sprint failed\n");
+			send_five_hundred(client_fd, server_fd);
+			return 1;
+		}
+
+		int bytes_sent = send(client_fd, response, response_len, 0);
 		if (bytes_sent == -1) {
 			printf("Send failed\n");
 			return 1;
@@ -125,5 +150,18 @@ int main() {
 
 	close(server_fd);
 
+	return 0;
+}
+
+int send_five_hundred(int client_fd, int server_fd) {
+	int bytes_sent = send(client_fd, "HTTP/1.1 500 Internal Server Error\r\n\r\n", 33, 0);
+	if (bytes_sent == -1) {
+		printf("Send failed\n");
+		return 1;
+	}
+
+	printf("Response sent\n");
+
+	close(server_fd);
 	return 0;
 }
